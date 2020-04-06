e6324d6c406777c9f45da55b928e3016d0aa0fa5
Nish26 <Nfactor26@gmail.com>
2020-04-06 17:45:36 +0530

    Added GetPropertyValue<T> and SetPropertyValue<T> extensions methods on object to get and set property values using reflection.

--- Pixel.Automation.Core/Extensions/TypeExtensions.cs
+++ Pixel.Automation.Core/Extensions/TypeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 
 namespace Pixel.Automation.Core.Extensions
 {
@@ -17,6 +18,39 @@ namespace Pixel.Automation.Core.Extensions
                 case false:
                     return type.Name;
             }          
+        }
+
+        public static T GetPropertyValue<T>(this object instance, string propertyName)
+        {
+            PropertyInfo sourceProperty = instance.GetType().GetProperty(propertyName);
+            if (sourceProperty == null)
+            {
+                throw new ArgumentException($"Property : {propertyName} doesn't exist on object of type : {instance.GetType()}");
+            }
+
+            //when type can be directly assigned
+            if (typeof(T).IsAssignableFrom(sourceProperty.PropertyType))
+            {
+                T targetPropertyValue = (T)sourceProperty.GetValue(instance);
+                return targetPropertyValue;
+            }
+            throw new Exception($"Data type : {typeof(T)} is not assignable from property {propertyName} on object");
+        }
+
+        public static  void SetPropertyValue<T>(this object instance, string propertyName, T valueToSet)
+        {
+            PropertyInfo targetProperty = instance.GetType().GetProperty(propertyName);
+            if (targetProperty == null)
+            {
+                throw new ArgumentException($"Property : {propertyName} doesn't exist on object of type : {instance.GetType()}");
+            }
+            if (targetProperty.PropertyType.IsAssignableFrom(typeof(T)))    //when type can be directly assigned
+            {
+                targetProperty.SetValue(instance, valueToSet);
+                return;
+            }
+
+            throw new Exception($"Data type : {typeof(T)} is not assignable to property {propertyName} on object");
         }
     }
 }
