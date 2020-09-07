a6918b1a8a2cc83778fd017eed1eed3c3da4cbbf
Nish26 <Nfactor26@gmail.com>
2020-09-07 15:53:29 +0530

    Make Web control scrapper great again !!

--- Pixel.Automation.Web.Scrapper/Startup.cs
+++ Pixel.Automation.Web.Scrapper/Startup.cs
@@ -1,57 +1,43 @@
-﻿using Microsoft.AspNet.SignalR;
-using Microsoft.Owin;
-using Microsoft.Owin.Cors;
-using Owin;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-[assembly: OwinStartup(typeof(Pixel.Automation.Web.Scrapper.Startup))]
 namespace Pixel.Automation.Web.Scrapper
 {
     public class Startup
     {
-
-        public void Configuration(IAppBuilder app)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
-            //app.UseCors(CorsOptions.AllowAll);         
-
-
-            //try
-            //{
-            //    GlobalHost.HubPipeline.AddModule(new ErrorHandlingPipelineModule());
-            //}
-            //catch (System.Exception)
-            //{
-
-
-            //}
-
-
-            //app.UseFileServer(new FileServerOptions()
-            //{
-            //    RequestPath = new PathString("/Scripts"),
-            //    EnableDirectoryBrowsing = true,
-            //});
-
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-            app.Map("/signalr", map =>
+            services.AddCors(options => options.AddPolicy("CorsPolicy",
+            builder =>
             {
-                // Setup the CORS middleware to run before SignalR.
-                // By default this will allow all origins. You can 
-                // configure the set of origins and/or http verbs by
-                // providing a cors options with a different policy.
-                map.UseCors(CorsOptions.AllowAll);
-                var hubConfiguration = new HubConfiguration
-                {
-                    // You can enable JSONP by uncommenting line below.
-                    // JSONP requests are insecure but some older browsers (and some
-                    // versions of IE) require JSONP to work cross domain
-                    EnableJSONP = true,
-                    EnableDetailedErrors = true                   
-                };
-                // Run the SignalR pipeline. We're not using MapSignalR
-                // since this branch already runs under the "/signalr"
-                // path.
-                map.RunSignalR(hubConfiguration);
+                builder.AllowAnyMethod().AllowAnyHeader()
+                       .WithOrigins("http://localhost:9143")
+                       .AllowCredentials();
+            }));
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ScrapperHub>("/scrapper");
             });
+
         }
     }
 }
