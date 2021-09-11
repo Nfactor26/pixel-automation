ba53730046e1982ac1dd8f89d8bf2a5de82fc7d0
Nish26 <Nfactor26@gmail.com>
2021-09-11 21:14:23 +0530

    Move DesignTimePrefabLoader out of Pixel.Automation.RunTime library

--- src/Pixel.Automation.RunTime/PrefabLoader.cs
+++ src/Pixel.Automation.RunTime/PrefabLoader.cs
@@ -3,7 +3,6 @@ using Pixel.Automation.Core;
 using Pixel.Automation.Core.Components.Prefabs;
 using Pixel.Automation.Core.Interfaces;
 using Pixel.Automation.Core.Models;
-using Pixel.Scripting.Editor.Core.Contracts;
 using Serilog;
 using System;
 using System.Collections.Generic;
@@ -125,27 +124,5 @@ namespace Pixel.Automation.RunTime
           return  this.projectFileSystem.LoadFile<PrefabReferences>(this.projectFileSystem.PrefabReferencesFile);
         }
     }
-
-    /// <summary>
-    /// DesignTimePrefabLoader configure some additional services such as ScriptEditor while loading a Prefab at design time.
-    /// </summary>
-    public class DesignTimePrefabLoader : PrefabLoader
-    {
-        public DesignTimePrefabLoader(IProjectFileSystem projectFileSystem) : base(projectFileSystem)
-        {
-          
-        }
-
-        protected override void ConfigureServices(IEntityManager parentEntityManager, IPrefabFileSystem prefabFileSystem)
-        {
-            //Process entity manager should be able to resolve any assembly from prefab references folder such as prefab data model assembly 
-            var scriptEngineFactory = parentEntityManager.GetServiceOfType<IScriptEngineFactory>();
-            scriptEngineFactory.WithAdditionalSearchPaths(prefabFileSystem.ReferencesDirectory);
-
-            //Similalry, Script editor when working with prefab input and output mapping script should be able to resolve references from prefab references folder
-            var scriptEditorFactory = parentEntityManager.GetServiceOfType<IScriptEditorFactory>();
-            scriptEditorFactory.AddSearchPaths(prefabFileSystem.ReferencesDirectory);
-        }
-    }
-
+    
 }
