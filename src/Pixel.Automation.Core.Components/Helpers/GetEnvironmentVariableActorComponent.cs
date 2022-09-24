14e8d5544209f5e6985d0441c7e1c2f8950c10e0
Nish26 <Nfactor26@gmail.com>
2022-09-24 13:56:23 +0530

    Add new actors to manage process environment variables

---
+++ src/Pixel.Automation.Core.Components/Helpers/GetEnvironmentVariableActorComponent.cs
@@ -0,0 +1,53 @@
+﻿using Dawn;
+using Pixel.Automation.Core.Arguments;
+using Pixel.Automation.Core.Attributes;
+using Serilog;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Pixel.Automation.Core.Components.Helpers
+{
+    /// <summary>
+    /// Retrieves the value of an environment variable from the current process.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    [ToolBoxItem("Get Env Variable", "Utility", iconSource: null, description: "Retrieve value of an environment variable from the current process", tags: new string[] { "get", "environment", "variable" })]
+    public class GetEnvironmentVariableActorComponent : ActorComponent
+    {
+        private readonly ILogger logger = Log.ForContext<GetEnvironmentVariableActorComponent>();
+
+        /// <summary>
+        /// Name of an environment variable
+        /// </summary>
+        [DataMember(Order = 10)]   
+        [Display(Name = "Env Variable", GroupName = "Input", Order = 10, Description = "Name of the environment variable")]
+        public Argument Variable { get; set; } = new InArgument<string>() { Mode = ArgumentMode.Default, CanChangeType = false };
+
+        /// <summary>
+        /// Retrieved value of environment variable
+        /// </summary>
+        [DataMember(Order = 20)]      
+        [Display(Name = "Value", GroupName = "Output", Order = 10, Description = "Retrieved value of environment variable")]
+        public Argument Value { get; set; } = new OutArgument<string?>() { Mode = ArgumentMode.Default, CanChangeType = false };
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public GetEnvironmentVariableActorComponent() : base("Get Environment Variable", "GetEnvironmentVariable")
+        {
+
+        }
+
+        public override async Task ActAsync()
+        {
+            var variableName = await this.ArgumentProcessor.GetValueAsync<string>(this.Variable);
+            Guard.Argument(variableName).NotNull().NotEmpty().NotWhiteSpace();
+            var variableValue = Environment.GetEnvironmentVariable(variableName);
+            await this.ArgumentProcessor.SetValueAsync(Value, variableValue);
+            logger.Information("Value of environment value {0} was retrieved", variableName);
+        }
+    }
+}
---
+++ src/Pixel.Automation.Core.Components/Helpers/SetEnvironmentVariableActorComponent.cs
@@ -0,0 +1,53 @@
+﻿using Dawn;
+using Pixel.Automation.Core.Arguments;
+using Pixel.Automation.Core.Attributes;
+using Serilog;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Pixel.Automation.Core.Components.Helpers
+{
+    /// <summary>
+    /// Creates, modifies, or deletes an environment variable stored in the current process.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    [ToolBoxItem("Set Env Variable", "Utility", iconSource: null, description: "Set environment variable", tags: new string[] { "set", "environment", "variable"})]
+    public class SetEnvironmentVariableActorComponent : ActorComponent
+    {
+        private readonly ILogger logger = Log.ForContext<SetEnvironmentVariableActorComponent>();
+
+        /// <summary>
+        /// Name of an environment variable
+        /// </summary>
+        [DataMember]     
+        [Display(Name = "Env Variable", GroupName = "Input", Order = 10, Description = "Name of the environment variable")]
+        public Argument Variable { get; set; } = new InArgument<string>() { Mode = ArgumentMode.Default, CanChangeType = false };
+
+        /// <summary>
+        /// Value to assign to environment variable
+        /// </summary>
+        [DataMember]      
+        [Display(Name = "Value", GroupName = "Input", Order = 20, Description = "Value to set for the environment variable")]
+        public Argument Value { get; set; } = new InArgument<string?>() { Mode = ArgumentMode.Default, CanChangeType = false };
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public SetEnvironmentVariableActorComponent() : base("Set Environment Variable", "SetEnvironmentVariable")
+        {
+
+        }
+
+        public override async Task ActAsync()
+        {
+            var variableName = await this.ArgumentProcessor.GetValueAsync<string>(this.Variable);
+            Guard.Argument(variableName).NotNull().NotEmpty().NotWhiteSpace();
+            var variableValue = await this.ArgumentProcessor.GetValueAsync<string?>(this.Value);
+            Environment.SetEnvironmentVariable(variableName, variableValue);
+            logger.Information("Value of environment value {0} was set", variableName);
+        }
+    }
+}
