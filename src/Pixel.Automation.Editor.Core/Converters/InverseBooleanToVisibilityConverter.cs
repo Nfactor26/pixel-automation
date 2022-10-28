a2f8ba0b6d3bee716e40e40c5b6c665300a77c67
Nish26 <Nfactor26@gmail.com>
2022-10-28 12:45:35 +0530

    Icons for settings, github repo and offline indicator

---
+++ src/Pixel.Automation.Editor.Core/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Pixel.Automation.Editor.Core.Converters
+{
+    public class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool result)
+            {
+                return result ?  Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
