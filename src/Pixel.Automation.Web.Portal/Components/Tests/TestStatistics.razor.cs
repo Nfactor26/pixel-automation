7e96ce36100e95c427c6ac75433641d415cb6cfe
Nish26 <Nfactor26@gmail.com>
2023-08-26 16:28:30 +0530

    Break down test statistics page in to component and page for reusability

---
+++ src/Pixel.Automation.Web.Portal/Components/Tests/TestStatistics.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Pixel.Automation.Web.Portal.Services;
+using Pixel.Automation.Web.Portal.ViewModels;
+using Pixel.Persistence.Core.Models;
+using Pixel.Persistence.Core.Request;
+using Pixel.Persistence.Core.Response;
+using System.Threading.Tasks;
+
+namespace Pixel.Automation.Web.Portal.Components.Tests;
+
+public partial class TestStatistics : ComponentBase
+{
+    [Inject]
+    public ITestResultService TestResultService { get; set; }
+
+    [Inject]
+    public ITestStatisticsService StatisticsService { get; set; }
+
+    [Parameter]
+    public string TestId { get; set; }
+
+    private TestStatisticsViewModel statisticsViewModel = new TestStatisticsViewModel();
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (!string.IsNullOrEmpty(TestId))
+        {
+            statisticsViewModel = await StatisticsService.GetTestStatisticsByTestId(TestId);
+        }
+    }
+
+    async Task<PagedList<TestResult>> GetTestResultsAsync(TestResultRequest testResultRequest)
+    {
+        testResultRequest.TestId = TestId;
+        var result = await TestResultService.GetTestResultsAsync(testResultRequest);
+        return result;
+    }
+}
---
+++ src/Pixel.Automation.Web.Portal/Pages/Tests/StatisticsPage.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Pixel.Automation.Web.Portal.Services;
+using Pixel.Automation.Web.Portal.ViewModels;
+using Pixel.Persistence.Core.Models;
+using Pixel.Persistence.Core.Request;
+using Pixel.Persistence.Core.Response;
+using System.Threading.Tasks;
+
+namespace Pixel.Automation.Web.Portal.Pages.Tests
+{
+    public partial class StatisticsPage : ComponentBase
+    {      
+        [Parameter]
+        public string TestId { get; set; }     
+    }
+}
