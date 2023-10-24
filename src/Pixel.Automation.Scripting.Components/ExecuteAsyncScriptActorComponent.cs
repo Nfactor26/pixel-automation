7cc5103a6a31772b45b3a0576eacd9cc7663ad83
Nish26 <Nfactor26@gmail.com>
2023-10-24 12:10:21 +0530

    Improve logging for Scripting components

--- src/Pixel.Automation.Scripting.Components/ExecuteAsyncScriptActorComponent.cs
+++ src/Pixel.Automation.Scripting.Components/ExecuteAsyncScriptActorComponent.cs
@@ -2,46 +2,49 @@
 using Pixel.Automation.Core.Attributes;
 using Pixel.Automation.Core.Components;
 using Pixel.Automation.Core.Interfaces;
+using Serilog;
 using System;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
-namespace Pixel.Automation.Scripting.Components
+namespace Pixel.Automation.Scripting.Components;
+
+/// <summary>
+/// Use <see cref="ExecuteAsyncScriptActorComponent"/> to execute custom scripts having asyncrhonous operations.
+/// </summary>
+[DataContract]
+[Serializable]
+[Scriptable("ScriptFile")]
+[Initializer(typeof(ScriptFileInitializer))]
+[ToolBoxItem("Script File (async)", "Scripting", iconSource: null, description: "Execute script that use async api's", tags: new string[] { "script"})]   
+public class ExecuteAsyncScriptActorComponent : ActorComponent
 {
-    /// <summary>
-    /// Use <see cref="ExecuteAsyncScriptActorComponent"/> to execute custom scripts having asyncrhonous operations.
-    /// </summary>
-    [DataContract]
-    [Serializable]
-    [Scriptable("ScriptFile")]
-    [Initializer(typeof(ScriptFileInitializer))]
-    [ToolBoxItem("Script File (async)", "Scripting", iconSource: null, description: "Execute script that use async api's", tags: new string[] { "script"})]   
-    public class ExecuteAsyncScriptActorComponent : ActorComponent
+    private readonly ILogger logger = Log.ForContext<ExecuteAsyncScriptActorComponent>();
+
+    protected string scriptFile;
+    [DataMember]
+    [System.ComponentModel.DisplayName("Script File")]      
+    public string ScriptFile
     {
-        protected string scriptFile;
-        [DataMember]
-        [System.ComponentModel.DisplayName("Script File")]      
-        public string ScriptFile
+        get => scriptFile;
+        set
         {
-            get => scriptFile;
-            set
-            {
-                scriptFile = value;
-                OnPropertyChanged();
-            }
+            scriptFile = value;
+            OnPropertyChanged();
         }
-
-        public ExecuteAsyncScriptActorComponent() : base("Execute Async Script", "ExecuteAsyncScript")
-        {
-
-        }
-
-        public override async Task ActAsync()
-        {
-            IScriptEngine scriptEngine = this.EntityManager.GetScriptEngine();
-            var action = await scriptEngine.CreateDelegateAsync<Func<IApplication, IComponent, Task>>(this.scriptFile);
-            await action(this.EntityManager.GetOwnerApplication(this), this);
-        }
-     
     }
+
+    public ExecuteAsyncScriptActorComponent() : base("Execute Async Script", "ExecuteAsyncScript")
+    {
+
+    }
+
+    public override async Task ActAsync()
+    {
+        IScriptEngine scriptEngine = this.EntityManager.GetScriptEngine();
+        var action = await scriptEngine.CreateDelegateAsync<Func<IApplication, IComponent, Task>>(this.ScriptFile);
+        await action(this.EntityManager.GetOwnerApplication(this), this);
+        logger.Information("Script file : '{0}' was executed", this.ScriptFile);
+    }
+ 
 }
--- src/Pixel.Automation.Scripting.Components/ExecuteInlineScriptActorComponent.cs
+++ src/Pixel.Automation.Scripting.Components/ExecuteInlineScriptActorComponent.cs
@@ -1,6 +1,7 @@
 ﻿using Pixel.Automation.Core;
 using Pixel.Automation.Core.Attributes;
 using Pixel.Automation.Core.Components;
+using Serilog;
 using System;
 using System.ComponentModel;
 using System.Runtime.Serialization;
@@ -18,6 +19,8 @@ namespace Pixel.Automation.Scripting.Components
     [ToolBoxItem("Script [Inline]", "Scripting", iconSource: null, description: "Assign value to a variable", tags: new string[] { "Assign", "Scripting" })]   
     public class ExecuteInlineScriptActorComponent : ActorComponent
     {
+        private readonly ILogger logger = Log.ForContext<ExecuteInlineScriptActorComponent>();
+
         protected string scriptFile;
         [DataMember]
         [DisplayName("Script File")]
@@ -39,7 +42,8 @@ namespace Pixel.Automation.Scripting.Components
         public override async Task ActAsync()
         {
             IScriptEngine scriptExecutor = this.EntityManager.GetScriptEngine();
-            _ = await scriptExecutor.ExecuteFileAsync(this.scriptFile);
+            _ = await scriptExecutor.ExecuteFileAsync(this.ScriptFile);
+            logger.Information("Script file : '{0}' was executed", this.ScriptFile);
         }
       
     }
