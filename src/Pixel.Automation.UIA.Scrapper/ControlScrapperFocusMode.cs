5065e42b225ee1d78babaa9c6d3c06b58be825ce
Nish26 <Nfactor26@gmail.com>
2025-11-23 21:24:37 +0530

    UIA Scrapper with focus support (#364)

---
+++ src/Pixel.Automation.UIA.Scrapper/ControlScrapperFocusMode.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+using Pixel.Automation.Core.Interfaces;
+using Pixel.Windows.Automation;
+
+namespace Pixel.Automation.UIA.Scrapper;
+
+public class ControlScrapperFocusMode : UIAControlScrapper
+{
+    public override string DisplayName => "UIA Scrapper : (Track Focus)";
+
+    public ControlScrapperFocusMode(IEventAggregator eventAggregator, IHighlightRectangleFactory highlightRectangleFactory, IScreenCapture screenCapture)
+        : base(eventAggregator, highlightRectangleFactory, screenCapture)
+    {
+    }
+
+    protected override AutomationElement GetTrackedElement()
+    { 
+        return AutomationElement.FocusedElement;
+    }
+}
